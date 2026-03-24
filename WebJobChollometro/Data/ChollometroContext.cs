3ff0c1a20dc29681defeb6543251fe65349f6aea
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-03-24 11:21:52 +0100

    up

---
+++ WebJobChollometro/Data/ChollometroContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebJobChollometro.Models;
+
+namespace WebJobChollometro.Data
+{
+    public class ChollometroContext : DbContext
+    {
+        public ChollometroContext(DbContextOptions<ChollometroContext> options) : base(options){}
+        public DbSet<Chollo> Chollos { get; set; }
+    }
+}
--- WebJobChollometro/Program.cs
+++ WebJobChollometro/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebJobChollometro.Data;
+using WebJobChollometro.Repositories;
+
+string connectionString = 
+    @"Data Source=sqlalvaro.database.windows.net;Initial Catalog=AZURETAJAMAR;Persist Security Info=True;User ID=adminsql;Password=Admin123;Encrypt=True;Trust Server Certificate=True";
+//Console.WriteLine("Bienvenido a nuestros chollos");
+//NECESITAMOS LA INYECCION DE DEPENDENCIAS
+var provider = new ServiceCollection()
+    .AddTransient<RepositoryChollometro>()
+    .AddDbContext<ChollometroContext>
+    (options => options.UseSqlServer(connectionString))
+    .BuildServiceProvider();
+//RECUPERAMOS EL REPOSITORY DE LA INYECCION
+RepositoryChollometro repo = provider.GetService<RepositoryChollometro>();
+//Console.WriteLine("Pulse ENTER para comenzar...");
+//Console.ReadLine();
+await repo.PopulateChollosAzureAsync();
+//Console.WriteLine("Proceso completado correctamente");
